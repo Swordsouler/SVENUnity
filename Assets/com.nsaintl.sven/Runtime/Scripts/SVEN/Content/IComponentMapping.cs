fa82a890ee58f68cb502d8d254daf75591fa3c16
Nicolas SL <nicolas.saint-leger@universite-paris-saclay.fr>
2025-07-16 14:53:17 +0200

    Update IComponentMapping.cs

--- Assets/com.nsaintl.sven/Runtime/Scripts/SVEN/Content/IComponentMapping.cs
+++ Assets/com.nsaintl.sven/Runtime/Scripts/SVEN/Content/IComponentMapping.cs
@@ -3,6 +3,7 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Linq;
 
 namespace Sven.Content
 {
@@ -10,5 +11,26 @@ namespace Sven.Content
     {
         public abstract string SemanticTypeName { get; }
         public static ComponentMapping ComponentMapping() => throw new NotImplementedException();
+        public static Type GetType(string semanticTypeName)
+        {
+            // get all IComponentMapping implementations and find the one with the matching SemanticTypeName == semancTypeName
+            var mappings = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => typeof(IComponentMapping).IsAssignableFrom(type) && !type.IsInterface && !type.IsAbstract);
+            foreach (var mapping in mappings)
+            {
+                // Get the SemanticTypeName property value
+                var semanticTypeNameProperty = mapping.GetProperty("SemanticTypeName");
+                if (semanticTypeNameProperty != null)
+                {
+                    var semanticTypeNameValue = semanticTypeNameProperty.GetValue(null) as string;
+                    if (semanticTypeNameValue == semanticTypeName)
+                    {
+                        return mapping;
+                    }
+                }
+            }
+            throw new ArgumentException($"No IComponentMapping found for semantic type name: {semanticTypeName}");
+        }
     }
 }
