f60f8b8a5cb616f629449ec3227733f1ee27e7f7
Swordsouler <swordsouler.nsl@gmail.com>
2026-01-14 16:36:28 +0100

    Revert "Update MappedComponents.cs"
    This reverts commit 75fc6b943d5c0988e1049c1b2cd83b01ef01ef98.

--- Assets/com.nsaintl.sven/Runtime/Scripts/SVEN/Content/MappedComponents.cs
+++ Assets/com.nsaintl.sven/Runtime/Scripts/SVEN/Content/MappedComponents.cs
@@ -386,8 +386,11 @@ namespace Sven.Content
         /// <returns>True if the component has the property, false otherwise.</returns>
         public static bool HasProperty(Type type, string propertyName)
         {
-            if (TryGetValue(type, out var componentDescription))
-                return componentDescription != null && componentDescription.CachedProperties.ContainsKey(propertyName);
+            if (Values.TryGetValue(type, out var componentDescription))
+                return componentDescription.CachedProperties.ContainsKey(propertyName);
+            else foreach (Type key in Values.Keys)
+                    if (key.IsAssignableFrom(type))
+                        return Values[key].CachedProperties.ContainsKey(propertyName);
             return false;
         }
 
