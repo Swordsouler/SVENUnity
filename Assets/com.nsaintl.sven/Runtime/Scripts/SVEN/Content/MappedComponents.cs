59b53e6d57f98236751bdcd93c7a7147af3b83a9
Swordsouler <swordsouler.nsl@gmail.com>
2026-01-14 16:36:30 +0100

    Revert "Update MappedComponents.cs"
    This reverts commit 33f1a46a66594b8d2f3ff8f26a4dea1bc3f8af52.

--- Assets/com.nsaintl.sven/Runtime/Scripts/SVEN/Content/MappedComponents.cs
+++ Assets/com.nsaintl.sven/Runtime/Scripts/SVEN/Content/MappedComponents.cs
@@ -308,7 +308,6 @@ namespace Sven.Content
             Dictionary<string, Tuple<int, Action<object>>> setters = new();
             if (Values.TryGetValue(component.GetType(), out var componentDescription))
             {
-                if (componentDescription == null) return setters;
                 foreach (Delegate del in componentDescription.Properties)
                     if (del.DynamicInvoke(component) is ComponentProperty propertyDescription)
                         setters.Add(propertyDescription.PredicateName, new(propertyDescription.Priority, propertyDescription.Setter));
@@ -321,7 +320,6 @@ namespace Sven.Content
             Dictionary<string, Tuple<int, Func<object>>> getters = new();
             if (Values.TryGetValue(component.GetType(), out var componentDescription))
             {
-                if (componentDescription == null) return getters;
                 foreach (Delegate del in componentDescription.Properties)
                     if (del.DynamicInvoke(component) is ComponentProperty propertyDescription)
                         getters.Add(propertyDescription.PredicateName, new(propertyDescription.Priority, propertyDescription.Getter));
